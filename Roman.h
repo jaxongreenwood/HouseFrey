df12a5a1fd7c7c39135ae163ba99a90ae8fe5de4
jaxongreenwood <jaxongreenwood@weber.mail.edu>
2017-10-26 14:00:45 -0600

    Added Roman class, along with private and public declaration

--- Roman.h
+++ Roman.h
@@ -4,8 +4,33 @@
 
 #ifndef HOUSEFREY_ROMAN_H
 #define HOUSEFREY_ROMAN_H
+#include <string>
+using namespace std;
 
+class Roman
+{
+private:
+    unsigned int value;
+    const string convetToRoman();
+    void convertFromRoman(const string&);
+public:
+    Roman();
+    Roman(const string&);
 
+    const Roman operator+(const Roman&);
+    const Roman operator+(const int);
+    friend Roman operator +(const int, const Roman&);
+    void operator +=(const Roman&);
+    void operator +=(const int);
+    Roman operator++();
+
+    friend ostream& operator << (ostream&, const Roman&);
+    friend istream& operator >> (istream&, Roman&);
+
+    friend bool checkTest(string, int, const Roman&);
+    friend void testOutput();
+
+};
 
 
 
