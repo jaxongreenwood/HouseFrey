db22f6cb98e550d7b6659a2787d964081a4618c6
Daryl <daryladams@mail.weber.edu>
2017-10-27 00:30:20 -0600

    Added Roman Class to project

--- Roman.h
+++ Roman.h
@@ -4,8 +4,31 @@
 
 #ifndef HOUSEFREY_ROMAN_H
 #define HOUSEFREY_ROMAN_H
+#include <string>
+using namespace std;
 
+class Roman
+{
+private:
+    unsigned int value;
+    string convertToRoman() const;
+    void convertFromRoman(const string&);
+public:
+    Roman();
+    Roman(const string&);
 
+    Roman operator+(const Roman&) const;
+    Roman operator+(const int) const;
+    friend Roman operator +(const int, const Roman&);
+    void operator +=(const int);
+    Roman operator++();
+
+    friend ostream& operator <<(ostream&, const Roman&);
+    friend istream& operator >>(istream&, Roman&);
+
+    friend bool checkTest(string, int, const Roman&);
+    friend void testOutput();
+};
 
 
 
